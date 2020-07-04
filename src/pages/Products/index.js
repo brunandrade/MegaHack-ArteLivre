4f057feee8e2733e228ce1a011bba1a36c53711f
brunandrade <bruna.card.andrade@gmail.com>
2020-07-04 16:34:54 -0300

    Página Arte Livre

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,11 +1,104 @@
 import React from 'react';
+import user from '../../assets/userImage.svg';
 import HeaderComponent from '../Components/HeaderComponent'
-import {Container} from'./styles';
+import {Container, SliderSmall, Content, ArtMenu, ArtContainer, Title, Description,
+  MenuSide, MenuItem, MenuButton, MenuText, MenuContent, ProductContainer, Bar, ProductPrice, ProductUser, UserImage,UserName, ImageContainer} from'./styles';
 
 function Products() {
   return (
       <Container>
           <HeaderComponent/>
+          <SliderSmall/>
+
+        <Content>
+          <ArtMenu>
+            <Title>Arte Livre</Title>
+            <Description>300 resultados</Description>
+
+            <MenuSide>
+              <MenuContent>
+                <MenuText>Localização</MenuText>
+                <MenuItem>São Paulo (100)</MenuItem>
+                <MenuItem>Rio de Janeiro (75)</MenuItem>
+                <MenuItem>Salvador (70)</MenuItem>
+                <MenuItem>Paraná (40)</MenuItem>
+                <MenuItem>Minas Gerais (32)</MenuItem>
+                <MenuButton>Ver todos</MenuButton>
+              </MenuContent>
+              
+              <MenuContent>
+                <MenuText>Lojas</MenuText>
+                <MenuItem>Gab Studio</MenuItem>
+                <MenuItem>Flores artesanato</MenuItem>
+                <MenuItem>2B Cadernos</MenuItem>
+                <MenuButton>Ver todos</MenuButton>
+              </MenuContent>
+             
+              <MenuContent>
+                <MenuText>Categorias</MenuText>
+                <MenuItem>Quadros</MenuItem>
+                <MenuItem>Ecobags</MenuItem>
+                <MenuItem>Roupas</MenuItem>
+                <MenuItem>Bolsas</MenuItem>
+                <MenuItem>Pinturas</MenuItem>
+                <MenuItem>Esculturas</MenuItem>
+                <MenuButton>Ver todos</MenuButton>
+              </MenuContent>
+              
+            </MenuSide>         
+          </ArtMenu>
+
+          <ArtContainer>
+            
+            <ProductContainer>
+              <Bar/>
+              <ProductPrice>R$ 30</ProductPrice>
+              <ProductUser>
+              <ImageContainer>
+                  <UserImage src={user} alt="Usuário"/>
+                </ImageContainer>
+                <UserName>Flores Artesanato</UserName>
+              </ProductUser>
+            </ProductContainer>
+
+            <ProductContainer>
+              <Bar/>
+              <ProductPrice>R$ 30</ProductPrice>
+              <ProductUser>
+              <ImageContainer>
+                  <UserImage src={user} alt="Usuário"/>
+                </ImageContainer>
+                <UserName>Flores Artesanato</UserName>
+              </ProductUser>
+            </ProductContainer>
+
+            <ProductContainer>
+              <Bar/>
+              <ProductPrice>R$ 30</ProductPrice>
+              <ProductUser>
+              <ImageContainer>
+                  <UserImage src={user} alt="Usuário"/>
+                </ImageContainer>
+                <UserName>Flores Artesanato</UserName>
+              </ProductUser>
+            </ProductContainer>
+
+            <ProductContainer>
+              <Bar/>
+              <ProductPrice>R$ 30</ProductPrice>
+              <ProductUser>
+                <ImageContainer>
+                  <UserImage src={user} alt="Usuário"/>
+                </ImageContainer>
+               
+                <UserName>Flores Artesanato</UserName>
+              </ProductUser>
+            </ProductContainer>
+            
+          </ArtContainer>
+        </Content>
+
+
       </Container>
   );
 }
--- src/pages/Products/styles.js
+++ src/pages/Products/styles.js
@@ -9,3 +9,172 @@ export const Container = styled.div`
     background: #EBEBEB;
     
 `; 
+
+export const SliderSmall = styled.div`
+    width: 100;
+    height: 241px;
+    background: #1F6CDB;
+    
+`; 
+
+export const Content = styled.div`
+    display: flex;   
+    flex-direction: row;
+    background: #EBEBEB;
+`; 
+
+export const ArtMenu = styled.div`
+    display: flex;   
+    flex-direction: column;
+    padding:20px;
+    margin-left:100px;
+`; 
+
+export const Title = styled.p`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 28px;
+    line-height: 33px;
+    letter-spacing: 0.015em;
+
+    color: #000000;
+`; 
+
+export const Description = styled.p`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 16px;
+    letter-spacing: 0.015em;
+    margin-bottom:20px;
+    color: #6C6C6C;
+`; 
+
+export const MenuSide = styled.div`
+    width:200px;
+    justify-content: start;
+`; 
+
+export const MenuContent = styled.div`
+    display: flex;   
+    flex-direction: column;
+    justify-content: normal;
+`; 
+
+export const MenuText = styled.p`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 19px;
+    letter-spacing: 0.015em;
+    color: #000000;
+    margin-bottom:10px;
+`; 
+
+export const MenuItem = styled.button`
+    background: transparent;
+    border:0;   
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 24px;
+    letter-spacing: 0.015em;
+    color: #6C6C6C;    
+    &:hover {
+        opacity:0.6;
+    } 
+`; 
+
+export const MenuButton = styled.button`
+    background: transparent;
+    border:0;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+    letter-spacing: 0.015em;
+    margin-bottom:20px;
+    color: #5697FA;
+
+    &:hover {
+        opacity:0.6;
+    } 
+`; 
+
+export const ArtContainer = styled.div`
+    display: flex;   
+    margin-left: 150px;
+   
+`; 
+
+export const ProductContainer = styled.div`  
+   width: 284px;
+   height: 510px;
+   background: #FFFFFF;
+   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
+   border-radius: 3px;
+   margin-top: 40px;
+   margin-left: 20px;
+  
+`; 
+
+export const Bar = styled.div`  
+   width: 283.92px;
+    height: 2.54px;
+    background: #EAEAEA;
+    margin-top:260px;
+`; 
+
+export const ProductPrice = styled.p`  
+   font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 28px;
+    line-height: 33px;
+    letter-spacing: 0.015em;
+    padding:20px;
+    color: #333333;
+`; 
+
+export const ProductUser = styled.div` 
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 20px;
+    align-items:center;
+    width: 284px;
+    height: 47px;
+    background: #1F6CDB;
+    border-radius: 3px; 
+    margin-top:150px;
+   
+`; 
+export const ImageContainer = styled.div`  
+    width: 52px;
+    height: 52px;
+    border: 1px solid;
+    border-color:#1F6CDB;
+    border-radius: 30px;
+    background-color:#FFF;
+    margin-bottom:20px;
+
+    
+`; 
+export const UserImage = styled.img`  
+    
+    
+`; 
+export const UserName = styled.p`  
+   font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    letter-spacing: 0.015em;
+    margin-right:20px;
+    color: #FFFFFF;
+`; 
